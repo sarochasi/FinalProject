7129f1c095b0bb36efa6dd92cbceed4103403e0a
Sarocha <rocha.right007@gmail.com>
2024-09-25 11:01:05 -0600

    Stub out JPA Project; Stub out User entity

---
+++ ReelSyncJPA/src/main/java/com/skilldistillery/media/entities/User.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.media.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+}
---
+++ ReelSyncJPA/src/test/java/com/skilldistillery/media/entities/UserTest.java
@@ -0,0 +1,47 @@
+package com.skilldistillery.media.entities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class UserTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("TodoJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+	
+//	@Test
+//	void test_User_entity_mapping() {
+//		assertNotNull(user);
+//		assertEquals("shaun", user.getUsername());
+//		assertEquals("shaun@winchester.co.uk", user.getEmail());	
+//		}
+
+
+}
