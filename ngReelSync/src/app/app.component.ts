b9ffe4e0560d36225c930c3919a89c4b23c95692
Ryan Hanson <hansonrj8322@gmail.com>
2024-09-26 13:40:50 -0600

    Create register component/html

--- ngReelSync/src/app/app.component.ts
+++ ngReelSync/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ngReelSync/src/app/components/register/register.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  newUser: User = new User();
+
+  constructor(private auth: AuthService, private router: Router) {
+  }
+
+  register(user: User): void {
+    console.log('Registering user:');
+    console.log(user);
+    this.auth.register(user).subscribe({
+      next: (registeredUser) => {
+        this.auth.login(user.username, user.password).subscribe({
+          next: (loggedInUser) => {
+            this.router.navigateByUrl('/home');
+          },
+          error: (problem) => {
+            console.error('RegisterComponent.register(): Error logging in user:');
+            console.error(problem);
+          }
+        });
+      },
+      error: (fail) => {
+        console.error('RegisterComponent.register(): Error registering account');
+        console.error(fail);
+      }
+    });
+  }
+}
