479489d06adacf9ff81421bb88fa2f5f4bdaf168
Ryan Hanson <hansonrj8322@gmail.com>
2024-10-07 09:14:00 -0600

    forward engineer

--- DB/mediadb.sql
+++ DB/mediadb.sql
@@ -574,9 +574,9 @@ INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (1, 1);
 INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (2, 1);
 INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (3, 1);
 INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (4, 1);
-INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (5, 2);
-INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (6, 2);
-INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (7, 2);
+INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (5, 3);
+INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (6, 3);
+INSERT INTO `media_has_playlist` (`media_id`, `playlist_id`) VALUES (7, 3);
 
 COMMIT;
 
